6d3f59114e8e914fd327081842052ca4c26ca6a8
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-10-29 22:35:03 +0300

    Add DioErrorX extensions for handle dio error connection

--- lib/api/api_repo.dart
+++ lib/api/api_repo.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print
-
+import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
 class ApiRepo {
   final String? url;
@@ -26,10 +25,15 @@ class ApiRepo {
       if (e.response?.statusCode == 404) {
         print("Error Status Code ${e.response?.statusCode}");
         onError!(e.response?.data);
+      } else if (e.isNoConnectionError) {
+        print("Error Status Code ${e.response?.statusCode}");
+        print("Error Response: ${e.response}");
+        print("Error Message: ${e.message}");
       } else {
-        print("Api Repo Error ${e.response?.statusCode}");
-        print("Api Repo Error ${e.response}");
-        print("Api Repo Error ${e.message}");
+        print("Api Repo Error Status Code: ${e.response?.statusCode}");
+        print("Api Repo Error Response: ${e.response}");
+        print("Api Repo Error Message: ${e.message}");
+        rethrow;
       }
     }
     // .catchError((error) {
@@ -38,4 +42,10 @@ class ApiRepo {
     //   }
     // });
   }
+}
+
+extension DioErrorX on DioError {
+  bool get isNoConnectionError =>
+      type == DioErrorType.other &&
+      error is SocketException; // import 'dart:io' for SocketException
 }
